8546f2480d7dd1ef57aed3678191c26d58543c4d
Piotr Starnawski <starnas88@gmail>
2023-06-18 20:04:25 +0200

    explore card rendering implementation

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ class Card:
                  card_time = "NA",
                  card_shape = "NA",
                  card_feature = "NA",
-                 solo_score = "NA"
+                 solo_score = "NA",
+                 card_number = "NA"
                  ):
 
         # variables
@@ -22,43 +23,64 @@ class Card:
         self.shape = card_shape
         self.feature = card_feature
         self.stars = solo_score
+        self.number = card_number
+        self.rendering = self.render()
 
     # card renderer
     def render(self):
 
-        print("-" * self.width)
+        # output list
+        out = []
 
-        if self.type == "draw":
+        # top border
+        out.append("-" * self.width)
 
+        # card type selector
+        if self.type == "explore":
+
+            name_lead = math.floor((self.width - 4 - len(self.name))/2)
+            name_padd = math.ceil((self.width - 4 - len(self.name))/2)
             feat_lead = math.floor((self.width - 4 - len(self.feature))/2)
             feat_padd = math.ceil((self.width - 4 - len(self.feature))/2)
 
-            print("| " + self.name + " " * (self.width - len(self.name) - 4) + " |")
-            print("| " +  " " * (self.width - 4) + " |")
-            print("| " + " " * feat_lead + self.feature + " " * feat_padd + " |")
-            print("| " +  " " * (self.width - 4) + " |")
+            out.append("| time: " + str(self.time) + " " * (self.width - 11) + " |")
+            out.append("| " + " " * (self.width - 4) + " |")
+            out.append("| " + " " * name_lead + self.name + " " * name_padd + " |")
+            out.append("| " +  " " * (self.width - 4) + " |")
+            out.append("| " + " " * feat_lead + self.feature + " " * feat_padd + " |")
+            out.append("| " +  " " * (self.width - 4) + " |")
 
             # check for 1 or 2 shapes
             if len(self.shape) == 3:
 
-                print("| " + " " * (self.width - 18) + self.shape[0] + " " * (self.width - 19) + " |")
-                print("| " + " " * (self.width - 18) + self.shape[1] + " " * (self.width - 19) + " |")
-                print("| " + " " * (self.width - 18) + self.shape[2] + " " * (self.width - 19) + " |")
+                out.append("| " + " " * (self.width - 18) + self.shape[0] + " " * (self.width - 19) + " |")
+                out.append("| " + " " * (self.width - 18) + self.shape[1] + " " * (self.width - 19) + " |")
+                out.append("| " + " " * (self.width - 18) + self.shape[2] + " " * (self.width - 19) + " |")
 
             if len(self.shape) == 6:
 
+                out.append("| " + " " * (self.width - 24) + self.shape[0] + " | " + self.shape[3] + " " * (self.width - 24) + " |")
+                out.append("| " + " " * (self.width - 24) + self.shape[1] + " | " + self.shape[4] + " " * (self.width - 24) + " |")
+                out.append("| " + " " * (self.width - 24) + self.shape[2] + " | " + self.shape[5] + " " * (self.width - 24) + " |")
 
+        # solo score
+        out.append("| " +  " " * (self.width - 4) + " |")
+        out.append("| " + " " * (self.width - 9) + self.number + "/41 |")
 
-        print("-" * self.width)
+        # bottom border
+        out.append("-" * self.width)
 
+        # return
+        return(out)
 
 # main
 if __name__ == "__main__":
 
     # generate a card_width
-    ec1 = Card(25, "draw", "example", 2, ["    []  ", "[][][]  ", "        "], "[H] or [W]", 20)
-    ec2 = Card(25, "draw", "example", 2, ["    []  ", "[][][]  ", "        ", "  []    ", "[][][]  ", "  []    "], "[F]", 20)
+    c07 = Card(25, "explore", "Great River", 1,
+                ["  []    ", "  []    ", "  []   C",
+                 "    []  ", "  [][]  ", "[][]    "],
+                "[W]", "NA", "07")
 
     # print test
-    ec1.render()
-    ec2.render()
+    print(*c07.rendering, sep = "\n")
